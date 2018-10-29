73e30fa025eb12c1be4d8e3cfcbbe4bb1085b959
unknown <hello@xrux.at>
2018-10-29 05:28:11 +0100

    implemented states and states sequence

---
+++ Assets/MyGame/Scripts/AdventureGame.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State actualState;
+
+	// Use this for initialization
+	void Start () {
+        actualState = startingState;
+        textComponent.text = actualState.GetStateStroy();
+	}
+
+    // Update is called once per frame
+    void Update () {
+        ManageState();
+	}
+
+    private State doTransition(State currentState, State nextState)
+    {
+//        count += 1;
+           
+
+        if (currentState.name == "Collect" && nextState.name == "Collect")
+        {
+            Debug.Log("Wolle +1");
+            return nextState;
+        }
+        if (currentState.name == "Knit" && nextState.name == "Knit")
+        {
+            Debug.Log("Wolle -2, Wasser +1");
+            return nextState;
+
+        }
+
+        return nextState;
+    }
+
+
+    private void ManageState()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            State[] nextStates = actualState.GetNextStates();
+            Debug.Log("States size 1");
+            if(nextStates.Length < 1)
+            {
+                return;
+            }
+            State nextState = nextStates[0];
+            actualState = doTransition(actualState, nextState);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            State[] nextStates = actualState.GetNextStates();
+            Debug.Log("States size 2");
+            if (nextStates.Length < 2)
+            {
+                return;
+            }
+            State nextState = nextStates[1];
+            actualState = doTransition(actualState, nextState);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            State[] nextStates = actualState.GetNextStates();
+            Debug.Log("States size 3");
+            if (nextStates.Length < 3)
+            {
+                return;
+            }
+            State nextState = nextStates[2];
+            actualState = doTransition(actualState, nextState); ;
+        }
+        else
+        {
+            Debug.Log("bin am leben");
+        }
+        textComponent.text = actualState.GetStateStroy();
+    }
+}
---
+++ Assets/MyGame/Scripts/State.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject{
+
+    private static readonly System.Random getrandom = new System.Random();
+
+    [TextArea(10,14)][SerializeField] string storyText;
+    public State[] nextStates;
+    public State[] randomStates;
+
+
+    public string GetStateStroy()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        if (randomStates.Length == 0)
+        {
+            return nextStates; 
+        }
+
+        int rnd = PickBiasedRandomState(0, 0, randomStates.Length);
+        State[] extendedArray = new State[nextStates.Length + 1];
+
+
+        Debug.Log("rnd " +rnd);
+        extendedArray[0] = randomStates[rnd];
+        for (int i = 0; i < nextStates.Length; i++)
+        {
+            extendedArray[i + 1] = nextStates[i];
+        }
+
+        Debug.Log("States are Merged");
+        return extendedArray;
+    }
+
+    int PickBiasedRandomState(int idxPrefered, int idxMin, int idxMax)
+    {
+        //return getrandom.NextDouble() < 0.8 ? idxPrefered : getrandom.Next(idxMin, idxMax);
+     
+        if (getrandom.NextDouble() < 0.8)
+        {
+            Debug.Log("< 0.8");
+            return idxPrefered;
+        }
+        Debug.Log("> 0.8");
+        return getrandom.Next(idxMin, idxMax);
+    }
+
+}
