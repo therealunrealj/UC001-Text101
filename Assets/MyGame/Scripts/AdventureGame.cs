cf52357045d4cf915bb11ab1bb405057db848cb8
unknown <hello@xrux.at>
2018-10-30 20:30:01 +0100

    start recfactoring

--- Assets/MyGame/Scripts/AdventureGame.cs
+++ Assets/MyGame/Scripts/AdventureGame.cs
@@ -2,18 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AdventureGameB : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
 using System;
 using System.Collections;
 using System.Collections.Generic;
